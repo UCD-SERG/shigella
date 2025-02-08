b1ce9ef58c2a6e7c795f4c8e114aa6dbfab252bd
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-07 23:11:08 -0800

    reorg

---
+++ R/shigella-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr any_of
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr rename
+#' @importFrom dplyr summarize
+## usethis namespace: end
+NULL
