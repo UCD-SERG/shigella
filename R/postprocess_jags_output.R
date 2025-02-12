1b61bf6f50efabeab964c25dec40b698a5864a92
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-11 22:07:49 -0800

    cleanup

--- R/postprocess_jags_output.R
+++ R/postprocess_jags_output.R
@@ -1,24 +1,32 @@
+#' Postprocess JAGS output
+#'
+#' @param jags_output output from [runjags::run.jags()]
+#'
+#' @returns a [tibble::tbl_df]
+#' @export
+#'
+#' @examples
 postprocess_jags_output <- function(jags_output) {
-  mcmc_list <- as.mcmc.list(jags_output)
+  mcmc_list <- coda::as.mcmc.list(jags_output)
   
-  mcmc_df <- ggs(mcmc_list)
+  mcmc_df <- ggmcmc::ggs(mcmc_list)
   
-  wide_predpar_df <- mcmc_df %>%
+  wide_predpar_df <- mcmc_df |>
     mutate(
-      parameter = sub("^(\\w+)\\[.*", "\\1", Parameter),
-      index_id = as.numeric(sub("^\\w+\\[(\\d+),.*", "\\1", Parameter)),
-      antigen_iso = as.numeric(sub("^\\w+\\[\\d+,(\\d+).*", "\\1", Parameter))
-    ) %>%
+      parameter = sub("^(\\w+)\\[.*", "\\1", .data$Parameter),
+      index_id = as.numeric(sub("^\\w+\\[(\\d+),.*", "\\1", .data$Parameter)),
+      antigen_iso = as.numeric(sub("^\\w+\\[\\d+,(\\d+).*", "\\1", .data$Parameter))
+    ) |>
     mutate(
       index_id = factor(index_id, labels = c(unique(dL_clean$index_id), "newperson")),
       antigen_iso = factor(antigen_iso, labels = unique(dL_clean$antigen_iso))
-    ) %>%
-    filter(index_id == "newperson") %>%
-    select(-Parameter) %>%
-    pivot_wider(names_from = "parameter", values_from = "value") %>%
-    rowwise() %>%
-    droplevels() %>%
-    ungroup() %>%
+    ) |>
+    filter(index_id == "newperson") |>
+    select(-Parameter) |>
+    pivot_wider(names_from = "parameter", values_from = "value") |>
+    rowwise() |>
+    droplevels() |>
+    ungroup() |>
     rename(r = shape)
   
   # Assuming wide_predpar_df is your data frame
@@ -29,11 +37,11 @@ postprocess_jags_output <- function(jags_output) {
   antigen_isos <- unique(curve_params$antigen_iso)
   attr(curve_params, "antigen_isos") <- antigen_isos
   
-  to_return <- curve_params %>%
+  to_return <- curve_params |>
     mutate(
       iter = Iteration,
       chain = Chain,
-    ) %>%
+    ) |>
     select(antigen_iso, iter, chain, y0, y1, t1, alpha, r)
   
   return(to_return)
