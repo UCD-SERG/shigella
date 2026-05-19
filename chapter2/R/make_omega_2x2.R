091d802371bf49f725a2575684ffc78f6fd20367
Kwan-Jenny <ksjlee@ucdavis.edu>
2026-05-19 17:28:56 +0000

    # Remove files

--- chapter2/R/make_omega_2x2.R
+++
@@ -1,6 +0,0 @@
-#' Build a 2x2 correlation matrix with off-diagonal rho
-#' @param rho numeric off-diagonal correlation
-#' @return 2x2 matrix
-make_omega_2x2 <- function(rho) {
-  matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2)
-}
